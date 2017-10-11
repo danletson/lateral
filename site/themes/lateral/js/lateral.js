76b6b28c3abc729eb99138e6fdf4bf5246329eb2
Daniel Letson <daniel.letson@gmail.com>
2017-10-10 21:25:44 -0700

    added a bunch of sounds, created skeleton for vue entry and player components

---
+++ site/themes/lateral/js/lateral.js
@@ -0,0 +1,78 @@
+Vue.config.delimiters = ['%%', '%%'];
+
+var data = {
+  activeID : 0,
+  activeFileURL : '',
+  nextFileURL : '',
+  previousFileURL : '',
+  showPlayer: false
+}
+
+Vue.component('player', {
+  template: `
+    <div class="vue-player">
+      <div class="track-title">
+        <span>Track Title</span>
+      </div>
+      <div class="previous-button">Previous</div>
+      <div class="play-button">Play / Pause</div>
+      <div class="next-button">Next</div>
+      <progress value="0" max="100">0</progress>
+      <div class="download-link">Download</div>
+      <audio><source src="" type="audio/mp3"></audio>
+    </div>
+    `,
+  props: ['activeSoundID'],
+  data: function(){
+    return data;
+  }
+});
+
+Vue.component('entries', {
+  template: `
+    <div class="main">
+      <slot></slot>
+    </div>
+  `,
+  data: function(){
+    return soundData
+  },
+  mounted() {
+    console.log(this.$children);
+  }
+});
+
+Vue.component('entry', {
+  template: `
+    <div class="entry entry--sound collapsed" :selected="true" :playing="false">
+      <header class="entry__header">
+        <span class="entry__number">{{ entryNumber }}</span> <span class="entry__artist">{{ artist }}</span>
+        <a href="#"><h2 class="entry__title">{{ title }}</h2></a>
+      </header>
+      <div class="entry__content">
+        <div class="entry__links">
+          <a href="" class="cta">Listen</a><a href="{{ soundURL }}" download class="cta">Download</a>
+        </div>
+        <div class="entry__text">
+        </div>
+      </div>
+    </div>
+  `,
+  data: function(){
+    return soundData
+  },
+  props: {
+    index: {required: true},
+    title: {required: true},
+    soundURL: {required: true},
+    permalink: {required: true},
+    artist: {required: true},
+    entryNumber: {required: true},
+    metaDescription: {required: true}
+  }
+});
+
+var app = new Vue({
+  el: '.vue-container',
+  data: soundData
+});
